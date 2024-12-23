1ca2b03ad064c5110ae0f01cc4da2f5dd96b0a12
Lucas-Haux <hauxlucas2005@gmail.com>
2024-12-22 21:14:56 -0800

    Add nixd lsp server

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   plugins = {
     lsp-lines = {
@@ -51,6 +51,25 @@
         eslint = {
           enable = true;
         };
+        nixd = {
+          enable = true;
+          settings =
+            let
+              flake = ''(builtins.getFlake "github:Lucas-Haux/Flake)""'';
+            in
+            {
+              nixpkgs = {
+                expr = "import ${flake}.inputs.nixpkgs { }";
+              };
+              formatting = {
+                command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+              };
+              options = {
+                nixos.expr = ''${flake}.nixosConfigurations.desktop.options'';
+                home_manager.expr = ''${flake}.homeConfigurations.desktop.options'';
+              };
+            };
+        };
         helm-ls = {
           enable = true;
           extraOptions = {
@@ -77,8 +96,10 @@
                   "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
                   "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
                   "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
-                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
+                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" =
+                    "*docker-compose*.{yml,yaml}";
+                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" =
+                    "*flow*.{yml,yaml}";
                 };
               };
             };
