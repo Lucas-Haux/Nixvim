2e8e2ffb5759a1e04cc0978dde3199574fceea75
Lucas-Haux <developer@lucas-haux.com>
2025-09-28 12:41:25 -0700

    Major lualine changes

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -1,4 +1,20 @@
-_: {
+{ pkgs, ... }:
+let
+  theme = pkgs.vimPlugins.nord-nvim;
+in
+{
+  extraConfigVim = # vimscript
+    ''
+      hi! link StatusLine Normal
+      hi! link StatusLineNC Normal
+    '';
+  extraConfigLua = # lua
+    ''
+      local custom_vscode = require('lualine.themes.nord')
+      custom_vscode.normal.c.bg = 'None'
+      require('lualine').setup { options = { theme  = custom_vscode } }
+    '';
+
   plugins.lualine = {
     enable = true;
     settings = {
@@ -14,60 +30,189 @@ _: {
             "alpha"
           ];
         };
-        theme = "nord";
+        componentSeparators = {
+          left = "";
+          right = "";
+        };
+        sectionSeparators = {
+          left = "";
+          right = "  ";
+
+        };
       };
       sections = {
         lualine_a = [
           {
             __unkeyed-1 = "mode";
-            icon = "";
+            separator.left = "";
+            padding = 2;
+            separator.right = "";
+
           }
         ];
         lualine_b = [
           {
+            separator.left = "";
             __unkeyed-1 = "branch";
-            icon = "";
+            separator.right = "";
+            color = {
+              bg = "#5e81ac";
+            };
           }
           {
             __unkeyed-1 = "diff";
             symbols = {
-              added = " ";
-              modified = " ";
-              removed = " ";
+              added = " ";
+              modified = " ";
+              removed = " ";
             };
+            color = {
+              bg = "#b48ead";
+            };
+            separator.right = "";
           }
-        ];
-        lualine_c = [
           {
             __unkeyed-1 = "diagnostics";
-            sources = [ "nvim_lsp" ];
+            sources = [ "nvim_diagnostic" ];
             symbols = {
-              error = " ";
-              warn = " ";
-              info = " ";
-              hint = "󰝶 ";
+              error = " ";
+              warn = " ";
+              info = " ";
             };
+            color = {
+              bg = "#d08770";
+            };
+            separator.right = "";
           }
-          { __unkeyed-1 = "navic"; }
         ];
+        lualine_c = [ { } ];
         lualine_x = [
           {
             __unkeyed-1 = "filetype";
-            icon_only = true;
-            separator = "";
-            padding = {
-              left = 1;
-              right = 0;
+            separator.left = "";
+            color = {
+              bg = "#5e81ac";
             };
           }
+        ];
+        lualine_y = [
           {
-            __unkeyed-1 = "filename";
-            path = 1;
+            __unkeyed-1 = "progress";
+            separator.left = "";
+            color = {
+              fg = "#2e3440";
+              bg = "#88c0d0";
+            };
+          }
+
+        ];
+        lualine_z = [
+          {
+            __unkeyed-1 = "location";
+            separator.right = "";
+            padding = 2;
           }
         ];
-        lualine_y = [ { __unkeyed-1 = "progress"; } ];
-        lualine_z = [ { __unkeyed-1 = "location"; } ];
       };
     };
+    # highlight = with theme; {
+    #   StatusLine = {
+    #     bg = nord0;
+    #   };
+    #
+    #   lualine_b_windows_active = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_windows_inactive = {
+    #     fg = nord6;
+    #     bg = nord1;
+    #   };
+    #   lualine_b_diff_modified_terminal = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_modified_inactive = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_terminal = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_inactive = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_modified_replace = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_modified_command = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_replace = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_command = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_modified_visual = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_modified_normal = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_modified_insert = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_visual = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_normal = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_removed_insert = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_terminal = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_inactive = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_replace = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_command = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_visual = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_normal = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    #   lualine_b_diff_added_insert = {
+    #     fg = nord6;
+    #     bg = nord10;
+    #   };
+    # };
+    #
   };
 }
